358253bc8295b11209a619f67512e96515c0a3a7
Hritika M Kucheriya <80444394+hritikamk@users.noreply.github.com>
2021-07-26 19:27:07 +0530

    Initial Commit

---
+++ contracts/UserAuth.sol
@@ -0,0 +1,28 @@
+pragma solidity ^0.4.23;
+
+contract UserAuth {
+	mapping (string => address) authAddr ;
+
+	event Create(string login);
+	event AuthChange(string login, address from, address to);
+	
+	function createAccount(string _login) public {
+		require(bytes(_login).length <= 32);
+		require(bytes(_login).length > 2);
+		require(authAddr[_login] == address(0));
+		authAddr[_login] = msg.sender;
+		
+		//emit Create(bytes32ToString(_login));
+		emit Create(_login);
+	}
+
+	function authAddress(string _login) view public returns (address){
+		return authAddr[_login];
+	}
+
+	function setAuthAddress(string _login, address _addr) public {
+		require(authAddr[_login] == msg.sender);
+		emit AuthChange(_login, authAddr[_login], _addr);
+		authAddr[_login] = _addr;
+	}
+}
