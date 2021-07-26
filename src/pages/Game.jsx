825a2fa062333ad4c99a9987e8ce666dad7ea702
Suryashankar Das <suryashankardas.2002@gmail.com>
2021-07-26 20:44:10 +0530

    dynamically add player

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,31 +1,48 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import Phaser from 'phaser';
 import { IonPhaser } from '@ion-phaser/react';
 import styles from '../styles/Game.module.css';
 import GameScene from '../scenes/GameScene';
 
-const Game = () => {
-  const state = {
-    initialize: true,
-    game: {
-      width: '100%',
-      height: '100%',
-      type: Phaser.AUTO,
-      scene: [GameScene],
-      physics: {
-        default: 'arcade',
-      },
-    },
-  };
+const game = {
+  width: '100%',
+  height: '100%',
+  type: Phaser.AUTO,
+  scene: [GameScene],
+  physics: {
+    default: 'arcade',
+  },
+  instance: null,
+};
 
-  const { initialize, game } = state;
-  return (
-    <IonPhaser
-      game={game}
-      initialize={initialize}
-      className={styles.fullScreen}
-    />
-  );
+function getInstance() {
+  if (game.instance) {
+    console.log('ready');
+    return Promise.resolve(game.instance);
+  } else {
+    console.log('waiting');
+    return new Promise((resolve) => {
+      const interval = setInterval(() => {
+        if (game.instance) {
+          clearInterval(interval);
+          resolve(game.instance);
+        }
+      });
+    }, 30);
+  }
+}
+
+const Game = () => {
+  const highScore = useRef(0);
+  useEffect(() => {
+    getInstance().then((instance) => {
+      instance.scene.scenes[0].events.emit('start-game', {
+        url: '/assets/character.png',
+        highScore,
+      });
+    });
+  }, []);
+  return <IonPhaser game={game} className={styles.fullScreen} />;
 };
 
 export default Game;
--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -4,30 +4,42 @@ class GameScene extends Phaser.Scene {
   constructor() {
     super({ key: 'GameScene' });
   }
+  init() {
+    this.loaded = false;
+    this.events.on('start-game', ({ url, highScore }) => {
+      this.load.image('player', url);
+      this.load.once(Phaser.Loader.Events.COMPLETE, () => {
+        this.player = this.createPlayer();
+        this.addCollider(this.blockOne);
+        this.addCollider(this.blockTwo);
+        this.loaded = true;
+      });
+      this.load.start();
+    });
+  }
   preload() {
     this.load.image('image', '/assets/block.png');
-    this.load.image('player', '/assets/character.png');
   }
   create() {
     this.cursors = this.input.keyboard.createCursorKeys();
-    this.player = this.createPlayer();
     this.blockOne = this.createBlock(this.genRandOne());
     this.blockTwo = this.createBlock(this.genRandTwo());
   }
   update() {
-    if (this.cursors.left.isDown) {
-      this.player.setVelocityX(-300);
-    } else if (this.cursors.right.isDown) {
-      this.player.setVelocityX(300);
-    } else {
-      this.player.setVelocityX(0);
+    if (this.loaded) {
+      if (this.cursors.left.isDown) {
+        this.player.setVelocityX(-300);
+      } else if (this.cursors.right.isDown) {
+        this.player.setVelocityX(300);
+      } else {
+        this.player.setVelocityX(0);
+      }
+      if (
+        this.blockOne.y > this.physics.world.bounds.height ||
+        this.blockTwo.y > this.physics.world.bounds.height
+      )
+        this.resetPos();
     }
-
-    if (
-      this.blockOne.y > this.physics.world.bounds.height ||
-      this.blockTwo.y > this.physics.world.bounds.height
-    )
-      this.resetPos();
   }
   createPlayer() {
     let player = this.physics.add.sprite(
@@ -35,11 +47,10 @@ class GameScene extends Phaser.Scene {
       this.physics.world.bounds.bottom,
       'player'
     );
-
-    player.setGravityY(1);
-    player.setCollideWorldBounds(true);
-    player.setBounce(0.5);
+    player.setGravityY(10);
     player.setScale(0.3);
+    player.setBounce(0.5);
+    player.setCollideWorldBounds(true);
 
     return player;
   }
@@ -49,15 +60,17 @@ class GameScene extends Phaser.Scene {
     platform.setGravityY(10);
     platform.setVelocityY(250);
     platform.setBounce(0.5);
-
+    return platform;
+  }
+  addCollider(platform) {
     this.physics.add.collider(this.player, platform, () => {
       alert('game over');
       this.blockOne.setVelocityY(250);
+      this.blockOne.setVelocityX(0);
       this.blockTwo.setVelocityY(250);
+      this.blockTwo.setVelocityX(0);
       this.resetPos();
     });
-
-    return platform;
   }
   resetPos() {
     this.blockOne.setY(0);
