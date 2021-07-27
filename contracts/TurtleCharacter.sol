35b28261eac9ca31903d2555dc318226173b1199
Dinesh B S <dineshbs444@gmail.com>
2021-07-27 17:28:51 +0530

    added prettier plugin for solidity and formatted smart contracts' code

--- contracts/TurtleCharacter.sol
+++ contracts/TurtleCharacter.sol
@@ -5,16 +5,15 @@ import "@chainlink/contracts/src/v0.6/ChainlinkClient.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract TurtleCharacter is ChainlinkClient, ERC721 {
-  
     bytes32 private ipfsHashOneBytes32;
     bytes32 private ipfsHashTwoBytes32;
     string public ipfsLink = "https://ipfs.io/ipfs/";
-    
+
     address private oracle;
     bytes32 private jobId;
     uint256 private fee;
     uint256 private uniqueTokenId = 0;
-    
+
     /**
      * Network: Rinkeby
      * Oracle - 0x3A56aE4a2831C3d3514b5D7Af5578E45eBDb7a40
@@ -25,44 +24,50 @@ contract TurtleCharacter is ChainlinkClient, ERC721 {
         setPublicChainlinkToken();
         oracle = 0x3A56aE4a2831C3d3514b5D7Af5578E45eBDb7a40;
         jobId = "187bb80e5ee74a139734cac7475f3c6e";
-        fee = 0.01 * 10 ** 18; // 0.01 LINK
+        fee = 0.01 * 10**18; // 0.01 LINK
     }
-    
+
     function requestRandomCharacter() public {
         ipfsLink = "https://ipfs.io/ipfs/";
         _safeMint(msg.sender, uniqueTokenId);
         requestIPFSHash();
     }
-     
-    function requestIPFSHash() public returns (bytes32 requestId) 
-    {
-        Chainlink.Request memory request = buildChainlinkRequest(jobId, address(this), this.fulfill.selector);   
-        request.add("get", "https://images-blend.herokuapp.com/");        
+
+    function requestIPFSHash() public returns (bytes32 requestId) {
+        Chainlink.Request memory request = buildChainlinkRequest(
+            jobId,
+            address(this),
+            this.fulfill.selector
+        );
+        request.add("get", "https://images-blend.herokuapp.com/");
         request.add("path", "IPFS_PATH");
-        
+
         return sendChainlinkRequestTo(oracle, request, fee);
     }
-    
-    function fulfill(bytes32 _requestId, bytes32 dataFromAPI) public recordChainlinkFulfillment(_requestId) returns (bytes32 requestId) 
-    {
+
+    function fulfill(bytes32 _requestId, bytes32 dataFromAPI) public recordChainlinkFulfillment(_requestId) returns (bytes32 requestId) {
         ipfsHashOneBytes32 = dataFromAPI;
 
-        Chainlink.Request memory request = buildChainlinkRequest(jobId, address(this), this.fulfillSecondRequest.selector);
+        Chainlink.Request memory request = buildChainlinkRequest(
+            jobId,
+            address(this),
+            this.fulfillSecondRequest.selector
+        );
         request.add("get", "https://images-blend.herokuapp.com/second");
         request.add("path", "IPFS_PATH");
-        
+
         return sendChainlinkRequestTo(oracle, request, fee);
     }
-    
+
     function fulfillSecondRequest(bytes32 _requestId, bytes32 dataFromAPI) public recordChainlinkFulfillment(_requestId) {
         ipfsHashTwoBytes32 = dataFromAPI;
         generateIPFSLink();
         setTokenURI(ipfsLink);
     }
-    
+
     function bytes32ToString(bytes32 _bytes32) public pure returns (string memory) {
         uint8 i = 0;
-        while(i < 32 && _bytes32[i] != 0) {
+        while (i < 32 && _bytes32[i] != 0) {
             i++;
         }
         bytes memory bytesArray = new bytes(i);
@@ -71,20 +76,29 @@ contract TurtleCharacter is ChainlinkClient, ERC721 {
         }
         return string(bytesArray);
     }
-    
+
     function generateIPFSLink() private {
         string memory part1 = bytes32ToString(ipfsHashOneBytes32);
         string memory part2 = bytes32ToString(ipfsHashTwoBytes32);
         ipfsLink = append(ipfsLink, part1, part2);
     }
-    
-    function append(string memory a, string memory b, string memory c) public pure returns (string memory) {
-    return string(abi.encodePacked(a, b, c));
+
+    function append(
+        string memory a,
+        string memory b,
+        string memory c
+    ) public pure returns (string memory) {
+        return string(abi.encodePacked(a, b, c));
     }
-    
+
     function withdrawLink() external {
-        LinkTokenInterface linkToken = LinkTokenInterface(chainlinkTokenAddress());
-        require(linkToken.transfer(msg.sender, linkToken.balanceOf(address(this))), "Unable to transfer LINK");
+        LinkTokenInterface linkToken = LinkTokenInterface(
+            chainlinkTokenAddress()
+        );
+        require(
+            linkToken.transfer(msg.sender, linkToken.balanceOf(address(this))),
+            "Unable to transfer LINK"
+        );
     }
 
     function setTokenURI(string memory _tokenURI) private {
--- contracts/UserAuth.sol
+++ contracts/UserAuth.sol
@@ -1,28 +1,28 @@
 pragma solidity ^0.6.6;
 
 contract UserAuth {
-	mapping (string => address) authAddr ;
+    mapping(string => address) authAddr;
 
-	event Create(string login);
-	event AuthChange(string login, address from, address to);
-	
-	function createAccount(string memory _login) public {
-		require(bytes(_login).length <= 32);
-		require(bytes(_login).length > 2);
-		require(authAddr[_login] == address(0));
-		authAddr[_login] = msg.sender;
-		
-		//emit Create(bytes32ToString(_login));
-		emit Create(_login);
-	}
+    event Create(string login);
+    event AuthChange(string login, address from, address to);
 
-	function authAddress(string memory _login) view public returns (address){
-		return authAddr[_login];
-	}
+    function createAccount(string memory _login) public {
+        require(bytes(_login).length <= 32);
+        require(bytes(_login).length > 2);
+        require(authAddr[_login] == address(0));
+        authAddr[_login] = msg.sender;
 
-	function setAuthAddress(string memory _login, address _addr) public {
-		require(authAddr[_login] == msg.sender);
-		emit AuthChange(_login, authAddr[_login], _addr);
-		authAddr[_login] = _addr;
-	}
+        //emit Create(bytes32ToString(_login));
+        emit Create(_login);
+    }
+
+    function authAddress(string memory _login) public view returns (address) {
+        return authAddr[_login];
+    }
+
+    function setAuthAddress(string memory _login, address _addr) public {
+        require(authAddr[_login] == msg.sender);
+        emit AuthChange(_login, authAddr[_login], _addr);
+        authAddr[_login] = _addr;
+    }
 }
