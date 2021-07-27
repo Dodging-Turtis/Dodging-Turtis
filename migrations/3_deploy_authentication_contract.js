f0a574bf1c9a4cdaa37a153101d4a432c74514d0
Dinesh B S <dineshbs444@gmail.com>
2021-07-28 02:13:33 +0530

    Minor changes

--- migrations/3_deploy_authentication_contract.js
+++ migrations/3_deploy_authentication_contract.js
@@ -1,6 +1,7 @@
 const TurtleGameUserAuth = artifacts.require("TurtleGameUserAuth");
 
-module.exports = async (deployer, network, [defaultAccount]) => {
-  await deployer.deploy(TurtleGameUserAuth);
-  let auth = await TurtleGameUserAuth.deployed();
+const nickName = "greg";
+
+module.exports = async (deployer) => {
+  await deployer.deploy(TurtleGameUserAuth, nickName);
 };
