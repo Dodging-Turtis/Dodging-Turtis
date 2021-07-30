bddabaece3e60e4e23645196b38dbe094efa2521
Suryashankar Das <suryashankardas.2002@gmail.com>
2021-07-30 21:08:19 +0530

    display list of user nfts

--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -1,40 +1,22 @@
 import '../styles/Start.module.css';
 import { useHistory } from 'react-router-dom';
 import { GameContext } from '../utils/web3';
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import NFT from '../components/NFT';
 
 function Start() {
   const history = useHistory();
-
   const { state, setState } = useContext(GameContext);
-
-  let items = [];
-  var nftArrayLength = state.nfts.length;
-  var i;
   const idwithURL = new Map();
 
-  console.log(nftArrayLength);
-
   useEffect(() => {
-    if (state.loded) {
+    if (state.loaded) {
       loadNFT();
-      for (i = 0; i < nftArrayLength; i++) {
-        items.push(
-          <NFT
-            id={i}
-            url={state.nfts[i]}
-            onClick={(i) => {
-              nftClicked(i);
-            }}
-          />
-        );
-        idwithURL.set(i, state.nfts[i]);
-      }
     }
-  }, [state.loded]);
+  }, [state.loaded, state.nfts.length, state.nfts]);
 
   function nftClicked(number) {
+    alert('clicked' + number);
     var url = idwithURL.get(number);
     setState({ ...state, selectedNFT: url });
   }
@@ -42,13 +24,25 @@ function Start() {
   const loadNFT = async () => {
     const nfts = [];
     const supply = await state.contract.methods.totalSupply().call();
+    console.log(supply);
     for (let i = 0; i < supply; i++) {
       const nft = await state.contract.methods.tokenByIndex(i).call();
       const uri = await state.contract.methods.tokenURI(nft).call();
       nfts.push(uri);
+      idwithURL.set(i, nfts[i]);
     }
     setState({ ...state, nfts });
   };
+
+  const items = state.nfts.map((url, i) => (
+    <NFT
+      id={i}
+      url={url}
+      onClick={(i) => {
+        nftClicked(i);
+      }}
+    />
+  ));
 
   return (
     <div>
