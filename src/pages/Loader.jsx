6950073f944a2f6aa3e437c9a40b7c72a6f239cd
Hardikag17 <54525688+Hardikag17@users.noreply.github.com>
2021-08-01 03:26:24 +0530

    added front page designs and animations

---
+++ src/pages/Loader.jsx
@@ -0,0 +1,10 @@
+import '../styles/Loader.css';
+
+function Loading() {
+  return (
+    <div id='loadingProgressG'>
+      <div id='loadingProgressG_1' class='loadingProgressG'></div>
+    </div>
+  );
+}
+export default Loading;
