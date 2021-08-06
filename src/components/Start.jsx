4bb7ff5be1013a86b1f526f4f53503a35ea742b3
Dinesh B S <dineshbs444@gmail.com>
2021-08-07 00:39:30 +0530

    fixed minor errors

--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -1,9 +1,9 @@
-import '../styles/Start.css';
-import { GameContext } from '../utils/web3';
-import { useContext, useEffect } from 'react';
-import NFT from '../components/NFT';
-import Load from '../components/Load';
-import Footer from '../components/Footer';
+import "../styles/Start.css";
+import { GameContext } from "../utils/web3";
+import { useContext, useEffect } from "react";
+import NFT from "../components/NFT";
+import Load from "../components/Load";
+import Footer from "../components/Footer";
 
 function Start() {
   const { state, setState } = useContext(GameContext);
@@ -27,12 +27,12 @@ function Start() {
           .call();
         const price = await state.contract.methods.turtlesForSale(nft).call();
         const url = await state.contract.methods.tokenURI(nft).call();
-        userNfts.push({ url, price, page: 'main', tokenId: nft });
+        userNfts.push({ url, price, page: "main", tokenId: nft });
       }
     } catch (e) {
-      console.log('nft fetch error');
+      console.log("nft fetch error");
     }
-    userNfts.push({ url: 'dummy', price: 0, page: 'main', tokenId: -1 });
+    userNfts.push({ url: "dummy", price: 0, page: "main", tokenId: -1 });
     setState({ ...state, userNfts });
   };
 
@@ -40,22 +40,23 @@ function Start() {
     state.userNfts.length > 0 ? (
       state.userNfts.map((nft) => <NFT key={nft.tokenId} nft={nft} />)
     ) : (
-      <div className='container-fluid position-absolute top-50 start-50 translate-middle'>
+      <div className="container-fluid position-absolute top-50 start-50 translate-middle">
         <Load />
       </div>
     );
 
   return (
-    <div className='container-fluid bg'>
+    <div className="container-fluid bg">
       <center>
-        <div className='start'>
+        <div className="start">
           <b>
             <h1>NFT's Connected to your wallet</h1>
           </b>
         </div>
         <div
-          className='d-flex justify-content-start'
-          style={{ flexWrap: 'wrap' }}>
+          className="d-flex justify-content-start"
+          style={{ flexWrap: "wrap", marginBottom: "10%" }}
+        >
           {items}
         </div>
       </center>
