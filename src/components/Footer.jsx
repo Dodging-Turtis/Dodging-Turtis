511faad46924644e3c2f623cc27bfcc2629d4e1f
Hardikag17 <54525688+Hardikag17@users.noreply.github.com>
2021-08-08 14:24:49 +0530

    added last changes to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,14 +3,21 @@ import '../styles/Footer.css';
 function Footer() {
   return (
     <div className='container-fluid position-fixed bottom-0 start-50 translate-middle-x footer'>
-      <h6>
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged.
-      </h6>
+      <div className='row'>
+        <div className='col-10'>
+          <h4></h4>
+          <h4>
+            Despite what most people believe, turtles can never come out of
+            their shells.
+          </h4>
+        </div>
+        <div className='col-2'>
+          <h2 style={{ color: '#242d3c' }}>Dodging Turtis</h2>
+        </div>
+        <center>
+          <div className='col-12'>Made with 🖤 by DTech</div>
+        </center>
+      </div>
     </div>
   );
 }
