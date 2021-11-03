b28ab7512bbbf6d8fb67c4f1c5d77ba44250c90f
Dinesh B S <dineshbs444@gmail.com>
2021-11-04 02:29:04 +0530

    removed minting logic from the contract

--- contracts/Turtis.sol
+++ contracts/Turtis.sol
@@ -2,26 +2,9 @@
 pragma solidity ^0.6.6;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@chainlink/contracts/src/v0.6/ChainlinkClient.sol";
 
-contract Turtis is ERC721, ChainlinkClient {
+contract Turtis is ERC721 {
   address public contractOwner; // Owner of this contract
-
-  // Bytes32 data related to IPFS Hash returned from the API
-  bytes32 private ipfsHashOneBytes32;
-  bytes32 private ipfsHashTwoBytes32;
-
-  string public ipfsLink = "ipfs://"; // Stores the IPFS link after the API call
-
-  // Chainlink Oracle, Job ID and fee required for making the API call using Chainlink
-  address private oracle;
-  bytes32 private jobId;
-  uint256 private fee;
-
-  uint256 private uniqueTokenId = 0; // Unique TokenID
-
-  string private apiBaseUrl; // Stores the base url of the api
-  string private apiSecondUrl; // API url for the second call
 
   address[] private users; // Address array to store all the users
 
@@ -45,26 +28,9 @@ contract Turtis is ERC721, ChainlinkClient {
   // Emitted when a turtle is put up for sale
   event TurtleUpForSale(uint256 tokenId);
 
-  /**
-   * Network: Matic Mumbai Testnet
-   * Oracle: 0xc8D925525CA8759812d0c299B90247917d4d4b7C
-   * Job ID: a7330d0b4b964c05abc66a26307047c0
-   * LINK address: 0x326C977E6efc84E512bB9C30f76E30c160eD06FB
-   * Fee: 0.01 LINK
-   */
-
   // Contructor is called when an instance of 'TurtleCharacter' contract is deployed
   constructor() public ERC721("TurtleCharacter", "TRTL") {
-    setChainlinkToken(0x326C977E6efc84E512bB9C30f76E30c160eD06FB);
-
-    uniqueTokenId = 0;
     contractOwner = msg.sender;
-    apiBaseUrl = "https://images-blend.herokuapp.com/";
-    apiSecondUrl = "https://images-blend.herokuapp.com/second/";
-
-    oracle = 0xc8D925525CA8759812d0c299B90247917d4d4b7C; // Address of the chainlink oracle
-    jobId = "a7330d0b4b964c05abc66a26307047c0"; // This Job gets data from an API in the form of a bytes32 variable
-    fee = 0.01 * 10**18; // 0.01 LINK (Fee for Chainlink Oracle for returning the required data)
   }
 
   // Modifer that checks to see if msg.sender == contractOwner
@@ -79,31 +45,6 @@ contract Turtis is ERC721, ChainlinkClient {
   // Function 'getUsers' returns the address array of users
   function getUsers() public view returns (address[] memory) {
     return users;
-  }
-
-  // Function 'setOracleAddress' sets a new oracle address
-  function setOracleAddress(address _oracleAddress) public onlyContractOwner {
-    oracle = _oracleAddress;
-  }
-
-  // Function 'setJobID' sets a new job ID
-  function setJobID(string memory _jobID) public onlyContractOwner {
-    jobId = stringToBytes32(_jobID);
-  }
-
-  // Function 'setFeeInLink' sets a new fee for the Chainlink Oracle
-  function setFeeInLink(uint256 _fee) public onlyContractOwner {
-    fee = _fee * 10**18;
-  }
-
-  // Function 'setApiBaseUrl' sets a new base URL link for the API
-  function setApiBaseUrl(string memory _url) public onlyContractOwner {
-    apiBaseUrl = _url;
-  }
-
-  // Function 'setApiSecondUrl' sets a new second URL link for the API
-  function setApiSecondUrl(string memory _url) public onlyContractOwner {
-    apiSecondUrl = _url;
   }
 
   // Function 'setHighScore' sets a new highScore for the user
@@ -145,143 +86,6 @@ contract Turtis is ERC721, ChainlinkClient {
     turtlesForSale[_tokenId] = _price;
 
     emit TurtleUpForSale(_tokenId);
-  }
-
-  // Function 'requestNewRandomTurtle' mints a new Turtle character
-  function requestNewRandomTurtle(string memory score) public {
-    setHighScore(score, msg.sender);
-    ipfsLink = "ipfs://";
-    _safeMint(msg.sender, uniqueTokenId++);
-    string memory apiLink = append(apiBaseUrl, "?score=", score);
-    string memory characterId = uint2str(totalSupply());
-    apiLink = append(apiLink, "&characterId=", characterId);
-    requestIPFSHash(apiLink);
-  }
-
-  // Function 'requestIPFSHash' requests a new IPFS hash from the API
-  function requestIPFSHash(string memory apiLink)
-    private
-    returns (bytes32 requestId)
-  {
-    Chainlink.Request memory request = buildChainlinkRequest(
-      jobId,
-      address(this),
-      this.fulfill.selector
-    );
-    request.add("get", apiLink);
-    request.add("path", "IPFS_PATH");
-
-    return sendChainlinkRequestTo(oracle, request, fee);
-  }
-
-  // Function 'fulfill' is called after the response for the API call is received
-  function fulfill(bytes32 _requestId, bytes32 dataFromAPI)
-    public
-    recordChainlinkFulfillment(_requestId)
-    returns (bytes32 requestId)
-  {
-    ipfsHashOneBytes32 = dataFromAPI;
-
-    Chainlink.Request memory request = buildChainlinkRequest(
-      jobId,
-      address(this),
-      this.fulfillSecondRequest.selector
-    );
-    request.add("get", apiSecondUrl);
-    request.add("path", "IPFS_PATH");
-
-    return sendChainlinkRequestTo(oracle, request, fee);
-  }
-
-  // Function 'fulfillSecondRequest' is called after the response for the API call is received
-  function fulfillSecondRequest(bytes32 _requestId, bytes32 dataFromAPI)
-    public
-    recordChainlinkFulfillment(_requestId)
-  {
-    ipfsHashTwoBytes32 = dataFromAPI;
-    generateIPFSLink();
-
-    emit DataReceivedFromAPI(ipfsLink);
-
-    setTokenURI(ipfsLink);
-
-    emit NewTurtleGenerated(uniqueTokenId - 1);
-  }
-
-  // Function 'bytes32ToString' convers a bytes32 variable to a string variable
-  function bytes32ToString(bytes32 _bytes32)
-    internal
-    pure
-    returns (string memory)
-  {
-    uint8 i = 0;
-    while (i < 32 && _bytes32[i] != 0) {
-      i++;
-    }
-    bytes memory bytesArray = new bytes(i);
-    for (i = 0; i < 32 && _bytes32[i] != 0; i++) {
-      bytesArray[i] = _bytes32[i];
-    }
-    return string(bytesArray);
-  }
-
-  // Function 'stringToBytes32' convers a string variable to a bytes32 variable
-  function stringToBytes32(string memory source)
-    internal
-    pure
-    returns (bytes32 result)
-  {
-    bytes memory tempEmptyStringTest = bytes(source);
-    if (tempEmptyStringTest.length == 0) {
-      return 0x0;
-    }
-    assembly {
-      result := mload(add(source, 32))
-    }
-  }
-
-  // Function 'generateIPFSLink' generates the IPFS link from the 2 API calls
-  function generateIPFSLink() private {
-    string memory ipfsHashPartOne = bytes32ToString(ipfsHashOneBytes32);
-    string memory ipfsHashPartTwo = bytes32ToString(ipfsHashTwoBytes32);
-    ipfsLink = append(ipfsLink, ipfsHashPartOne, ipfsHashPartTwo);
-    ipfsLink = append(ipfsLink, "/metadata", ".json");
-  }
-
-  // Function 'append' appends 3 strings
-  function append(
-    string memory a,
-    string memory b,
-    string memory c
-  ) internal pure returns (string memory) {
-    return string(abi.encodePacked(a, b, c));
-  }
-
-  // Function 'uint2str' converts a uint256 variable to a string variable
-  function uint2str(uint256 _i)
-    internal
-    pure
-    returns (string memory _uintAsString)
-  {
-    if (_i == 0) {
-      return "0";
-    }
-    uint256 j = _i;
-    uint256 len;
-    while (j != 0) {
-      len++;
-      j /= 10;
-    }
-    bytes memory bstr = new bytes(len);
-    uint256 k = len;
-    while (_i != 0) {
-      k = k - 1;
-      uint8 temp = (48 + uint8(_i - (_i / 10) * 10));
-      bytes1 b1 = bytes1(temp);
-      bstr[k] = b1;
-      _i /= 10;
-    }
-    return string(bstr);
   }
 
   // Function 'setTokenURI' sets the Token URI for the ERC721 standard token
