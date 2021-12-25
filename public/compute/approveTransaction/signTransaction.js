977ad8b7938e7cb75d702f5ecc30e26bf557dd9b
Dinesh B S <dineshbs444@gmail.com>
2021-12-25 16:25:00 +0530

    logic for signing transaction is added

---
+++ public/compute/approveTransaction/signTransaction.js
@@ -0,0 +1,18 @@
+const Web3 = require("web3");
+require("dotenv").config({ path: __dirname + "./../../../.env" });
+
+var web3 = new Web3("http://localhost:8545");
+var privateKey = process.env.METAMASK_PRIVATE_KEY;
+
+export async function generateSig(message) {
+  // console.log("Hashed message is " + web3.utils.soliditySha3(message));
+  const sig = web3.eth.accounts.sign(
+    web3.utils.soliditySha3(message),
+    privateKey
+  );
+
+  // console.log("Signature : \n " + JSON.stringify(sig));
+  const signature = sig.signature;
+  // console.log("Sig: ", signature);
+  return signature;
+}
