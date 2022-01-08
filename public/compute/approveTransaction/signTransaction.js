ce9d957a9026c169b91551d3612a99781be23a77
Dinesh B S <dineshbs444@gmail.com>
2022-01-08 15:43:59 +0530

    update api & sign transaction code

--- public/compute/approveTransaction/signTransaction.js
+++ public/compute/approveTransaction/signTransaction.js
@@ -4,17 +4,15 @@ require("dotenv").config({ path: __dirname + "./../../../.env" });
 var web3 = new Web3("http://localhost:8545");
 var privateKey = process.env.METAMASK_PRIVATE_KEY;
 
-async function generateSig(message) {
+async function generateSig(score, walletAddress, tokenURI) {
   // console.log("Hashed message is " + web3.utils.soliditySha3(message));
   const sig = web3.eth.accounts.sign(
-    web3.utils.soliditySha3(message),
+    web3.utils.soliditySha3(score, walletAddress, tokenURI),
     privateKey
   );
 
   // console.log("Signature : \n " + JSON.stringify(sig));
-  const signature = sig.signature;
-  // console.log("Sig: ", signature);
-  return signature;
+  return sig;
 }
 
 module.exports = { generateSig };
