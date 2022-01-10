93c769237862a7fa4ec3d99b3179e4d6b2bc2132
Suryashankar Das <suryashankardas.2002@gmail.com>
2022-01-11 01:24:32 +0530

    remove redux

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,12 @@
 import type { AppProps } from 'next/app';
-import { Provider } from 'react-redux';
-import { store } from '../src/redux';
-import Wrapper from '../components/Wrapper';
+import { StoreProvider } from '../store';
 import '../styles/main.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Provider store={store}>
-      <Wrapper>
-        <Component {...pageProps} />
-      </Wrapper>
-    </Provider>
+    <StoreProvider>
+      <Component {...pageProps} />
+    </StoreProvider>
   );
 }
 
