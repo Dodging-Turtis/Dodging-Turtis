ec33e1b0da19085c7f0fa3a9d5b8e703fa8bcf42
Suryashankar Das <suryashankardas.2002@gmail.com>
2022-02-09 02:22:16 +0530

    add some more styling to market page

--- components/marketlayout/index.tsx
+++ components/marketlayout/index.tsx
@@ -35,26 +35,40 @@ const MarketLayout = () => {
   ));
 
   return (
-    <div>
-      <select value={store.sortOrder} onChange={sortOrderhandler}>
-        <option value='0'>price low to high</option>
-        <option value='1'>price high to low</option>
-        <option value='2'>latest first</option>
-        <option value='3'>oldest first</option>
-      </select>
+    <div className='pt-5'>
+      <div className='flex justify-items-center items-center gap-1'>
+        <div className='text-lg'>Sort By:</div>
+        <select
+          className='p-2 rounded-lg bg-whiteish text-blue'
+          value={store.sortOrder}
+          onChange={sortOrderhandler}>
+          <option value='0' className='p-2 rounded-lg'>
+            price low to high
+          </option>
+          <option value='1' className='p-2 rounded-lg'>
+            price high to low
+          </option>
+          <option value='2' className='p-2 rounded-lg'>
+            latest first
+          </option>
+          <option value='3' className='p-2 rounded-lg'>
+            oldest first
+          </option>
+        </select>
+      </div>
       <div className='h-full p-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-12'>
         {isLoading ? dummyCards : cards}
       </div>
-      <div className='flex justify-center items-center'>
+      <div className='flex justify-center items-center text-lg text-blue'>
         <button
           onClick={() => store.page--}
-          className='bg-whiteish p-2 m-1 rounded-lg'>
-          previous
+          className='bg-whiteish px-3 py-2 m-1 rounded-lg'>
+          Previous
         </button>
         <button
           onClick={() => store.page++}
-          className='bg-whiteish p-2 m-1 rounded-lg'>
-          next
+          className='bg-whiteish px-3 py-2 m-1 rounded-lg'>
+          Next
         </button>
       </div>
     </div>
