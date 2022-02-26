d78b3f3b74337f1444fd2d4d5a037c52559d5c63
Hardikag17 <hardikag17@gmail.com>
2022-02-27 01:11:55 +0530

    Fix: basic database connection established

--- backend/mongoose/index.js
+++ backend/mongoose/index.js
@@ -1,51 +1,43 @@
+const ObjectId = require('mongodb').ObjectId;
+import dbConnect from '../../lib/mongodb';
 const mongoose = require('mongoose');
-const multer = require('multer');
-const fs = require('fs');
-const path = require('path');
-import './db';
-const auth = require('./models/auth');
-const turtle = require('./models/turtle');
-
-var storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'uploads');
-  },
-  filename: (req, file, cb) => {
-    cb(null, file.fieldname + '-' + Date.now());
-  },
-});
-
-var upload = multer({ storage: storage });
+import auth from './models/auth';
 
 export default async function handler(req, res) {
-  res.status(404).json({ dev: 'Dodging Turtis v2.0' });
-  if (req.method == 'GET') {
-    auth.find(
-      { wallet_address: req.body.wallet_address },
-      function (err, found) {
-        if (err) {
-          res.send('user not found');
-        } else {
-          res.send(found);
-        }
-      }
-    );
-  } else if (req.method == 'POST') {
-    upload.single('avatar');
-    const new_user = new auth({
-      wallet_address: req.body.wallet_address,
-      img: req.body.img,
-      username: req.body.username,
-      nickname: req.body.nickname,
-    });
+  await dbConnect();
 
-    new_user.save(function (err) {
-      if (!err) {
-        res.send('you are successfully registered');
-      } else {
-        res.send(err);
-      }
-    });
+  if (req.method == 'GET') {
+    try {
+      let user = await auth.find({ wallet_address: req.body.wallet_address });
+
+      return res.json({
+        message: JSON.parse(JSON.stringify(user)),
+        success: true,
+      });
+    } catch (error) {
+      return res.json({
+        message: new Error(error).message,
+        success: false,
+      });
+    }
+  } else if (req.method == 'POST') {
+    try {
+      await auth.create({
+        wallet_address: req.body.wallet_address,
+        username: req.body.username,
+        nickname: req.body.nickname,
+      });
+
+      return res.json({
+        message: 'Post added successfully',
+        success: true,
+      });
+    } catch (error) {
+      return res.json({
+        message: new Error(error).message,
+        success: false,
+      });
+    }
   } else {
     res.status(200).json({ error: 'Wrong route fetching' });
   }
--- backend/mongoose/models/auth.js
+++ backend/mongoose/models/auth.js
@@ -1,32 +1,28 @@
 const mongoose = require('mongoose');
 
 // schema for Registering/Validating the user
-const authSchema = new mongoose.Schema({
-  wallet_address: {
-    type: String,
-    required: true,
-  },
-  img: {
-    data: Buffer,
-    contentType: String,
-  },
-  username: {
-    type: String,
-    immutable: true,
-    required: true,
-    unique: true,
-  },
-  nickname: {
-    type: String,
-    required: true,
-  },
-  createdAt: {
-    type: Date,
-    immutable: true,
-    default: () => {
-      Date.now();
+const authSchema = new mongoose.Schema(
+  {
+    wallet_address: {
+      type: String,
+      required: true,
+    },
+    img: {
+      data: Buffer,
+      contentType: String,
+    },
+    username: {
+      type: String,
+      immutable: true,
+      required: true,
+      unique: true,
+    },
+    nickname: {
+      type: String,
+      required: true,
     },
   },
-});
+  { timestamps: true }
+);
 
 export default mongoose.models.auth || mongoose.model('auth', authSchema);
