e12129cbc4cd3e3738c9298c53ca4edc675f6a64
Dinesh B S <dineshbs444@gmail.com>
2022-03-19 15:32:13 +0530

    feat: added spatially separated turtle components

--- backend/signtransaction/generateTurtle.js
+++ backend/signtransaction/generateTurtle.js
@@ -1,22 +1,24 @@
-import images from 'images';
+import images from "images";
 
-let folder = './public/assets/img/turtle_components';
+let folder = "./public/assets/img/turtle_components_gen";
 let components = [
-  'frontFeet',
-  'hindFeet',
-  'heads',
-  'tails',
-  'innerShells',
-  'outerShells',
-  'eyes',
+  "frontFeet",
+  "hindFeet",
+  "heads",
+  "tails",
+  "innerShells",
+  "outerShells",
+  "eyes",
 ];
-let file_extension = '.png';
+let file_extension = ".png";
 let total_components = components.length;
 let images_per_component = 5;
 let total_number_of_breeds = 10;
 
 export async function generateRandomTurtle() {
-  let breedRandomNumber = Math.floor(Math.random() * total_number_of_breeds + 1);
+  let breedRandomNumber = Math.floor(
+    Math.random() * total_number_of_breeds + 1
+  );
   let randomNumbers = new Array();
   for (let i = 0; i < total_components; i++) {
     let num = Math.floor(Math.random() * images_per_component + 1);
@@ -27,7 +29,14 @@ export async function generateRandomTurtle() {
 
   for (let i = 0; i < total_components; i++) {
     let imageUrl =
-      folder + '/breed_' + breedRandomNumber + '/' + components[i] + '/' + randomNumbers[i] + file_extension;
+      folder +
+      "/breed_" +
+      breedRandomNumber +
+      "/" +
+      components[i] +
+      "/" +
+      randomNumbers[i] +
+      file_extension;
     if (i == 0) {
       blendedImage = await images(imageUrl);
     } else {
@@ -37,7 +46,7 @@ export async function generateRandomTurtle() {
 
   return {
     componentIndicesArray: randomNumbers,
-    imgdata: blendedImage.toBuffer('png'),
+    imgdata: blendedImage.toBuffer("png"),
     breed: breedRandomNumber,
   };
 }
@@ -48,10 +57,10 @@ export async function dummyTurtle() {
     let num = Math.floor(Math.random() * count_images_per_component[i] + 1);
     randomNumbers.push(num.toString());
   }
-  const blendedImage = await images('./public/assets/character.png');
+  const blendedImage = await images("./public/assets/character.png");
   return {
     componentIndicesArray: randomNumbers,
-    imgdata: blendedImage.toBuffer('png'),
+    imgdata: blendedImage.toBuffer("png"),
     breed: 1,
   };
 }
